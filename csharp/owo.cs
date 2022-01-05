7fc07b73ff210803253530f84f0deb0c435539d3
charlottecross1998 <29734551+CharlotteCross1998@users.noreply.github.com>
2022-01-05 21:36:02 +0000

    Added csharp support

---
+++ csharp/owo.cs
@@ -0,0 +1,15 @@
+using System;
+namespace owo
+{
+	class owo
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("__        ___           _    _        _   _     _      ____");
+			Console.WriteLine("\\ \\      / / |__   __ _| |_ ( ) ___  | |_| |__ (_) ___ |___\\");
+			Console.WriteLine(" \\ \\ /\\ / /| '_ \\ / _\\`| __|// / __| | __| '_ \\| |/ __|  / /");
+			Console.WriteLine("  \\ V  V / | | | | (_| | |_    \\__ \\ | |_| | | | |\\__ \\  |_|");
+			Console.WriteLine("   \\_/\\_/  |_| |_|\\__,_|\\__|   |___/ \\___|_| |_|_|/___/  (_)");
+		}
+	}
+}
